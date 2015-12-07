d7bd4ee7767468d29c15a69fd9d7784cdfca53f7
Andrew Odewahn <andrew@odewahn.com>
2015-12-07 06:01:02 -0500

    kinda working

--- src/components/features.react.js
+++ src/components/features.react.js
@@ -7,6 +7,11 @@ var ThemeManager = require('material-ui/lib/styles/theme-manager');
 var LightRawTheme = require('material-ui/lib/styles/raw-themes/light-raw-theme');
 var Colors = require('material-ui/lib/styles/colors');
 
+const FontIcon = require('material-ui/lib/font-icon');
+const IconButton = require('material-ui/lib/icon-button');
+
+
+
 /*
 Example of how to render colors and styles:
 
@@ -14,7 +19,7 @@ Example of how to render colors and styles:
 * http://www.material-ui.com/#/customization/themes
 
 */
- 
+
 
 module.exports = React.createClass({
 
@@ -36,19 +41,39 @@ module.exports = React.createClass({
   componentWillMount: function() {
     console.log("mounting component")
     var newMuiTheme = ThemeManager.modifyRawThemePalette(this.state.muiTheme, {
-      primary1Color: Colors.deepOrange500,
+      //primary1Color: Colors.deepOrange500,
       //canvasColor: Colors.deepOrange500,
     });
     this.setState({muiTheme: newMuiTheme});
   },
 
   render: function() {
+
+
     return (
       <div className="container-fluid">
         <div className="row">
-          <div className="col-md-12">
+          <div className="col-sm-12">
             <AppBar title="Andrew Odewahn" />
           </div>
+        </div>
+        <div className="row">
+            <div className="col-sm-3">
+              <img className="headshot" src="images/headshot.jpg"/>
+              <br/>
+                <div className="social">
+                  <i className="fa fa-github socialIcon"></i>
+                  <i className="fa fa-twitter socialIcon"></i>
+                  <i className="fa fa-envelope socialIcon"></i>
+                </div>
+            </div>
+            <div className="col-sm-9 bioText">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac velit nec risus iaculis consectetur eget et turpis. In hac habitasse platea dictumst. Aliquam dictum maximus sem vitae convallis. In sapien enim, fringilla tempus est non, hendrerit feugiat dolor. Phasellus ac convallis justo. Vestibulum quis dapibus quam. Sed gravida lectus in suscipit aliquam. Cras id metus ut orci consectetur suscipit eu sed arcu.
+
+
+
+
+            </div>
         </div>
         <div className="row">
           <div className="col-md-12">
